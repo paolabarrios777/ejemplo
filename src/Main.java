8f82de18931d0e38f1e20979ba0cb8c35cc56640
Alemania195 <jbarraganramirez0@gmail.com>
2023-10-17 16:10:08 -0500

    primer compromiso

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+class Arreglo {
+    public static void main(String[] args) {
+        String[] comidas = {"pollo,res,cerdo,salchicha,pescado,pavo"};
+        for (String i:comidas) {
+            System.out.println("comidas:" +i);
+        }
+
+    }
+}
